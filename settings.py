e84a929cf01358f38b580fa39fd0b6381214c5f8
Sveta5612 <sveta5612@BK.RU>
2023-03-21 12:38:44 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = 'C:\chromedriver/chromedriver.exe'
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 's5612@bk.ru'
+    valid_password = 'Sv123456'
+    valid_email_for_reg = 's5612@bk.ru'
+    invalid_email = 'kormakovv@mail.ru'
+    invalid_password = 'Kr123456'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Татьяна'
+    l_name = 'Прокопьева'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'и'
+    latin_generate_string = 'ssttww'
+    chinese_chars = '我不会说汉语请做不辣的怎么样你会说英语'
+    special_chars = '!@#$$%^&*()_+_+=//{}'
+    numbers = 987
+    empty = ''
+    passw1 = 'Sv123456'
+    passw2 = 'Qw12345678Wq543'
+    passw3 = 'Qa12345678Er12345978'
